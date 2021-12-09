610e674a3863471bd495773301c8ca858d66a914
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-12-09 11:17:38 +0530

    Add files via upload

---
+++ stepper motor with delay.asm
@@ -0,0 +1,56 @@
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+#start=stepper_motor.exe#
+       
+name "motorc"
+
+ 
+START: MOV BX, offset datcw
+
+       MOV SI, 0
+
+NEXT_STEP:
+
+WAIT: IN AL,07H
+
+    TEST AL,10000000b
+
+    JZ WAIT
+
+    MOV AL, [BX][SI]
+
+    OUT 7, AL     
+    
+    
+    MOV CX, 005h
+    
+    UP:
+    
+    NOP
+    
+    NOP
+    
+    LOOP UP
+    
+    
+    INC SI
+    
+    CMP SI, 4
+    
+    JC NEXT_STEP
+
+    MOV SI, 0 
+    
+    JMP NEXT_STEP
+    
+
+
+;        7654 3210
+datcw db 0000_0110b
+
+      db 0000_1100b 
+      
+      db 0000_1001b
+
+      db 0000_0011b 
